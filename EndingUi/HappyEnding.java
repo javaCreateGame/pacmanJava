91a14b7ff4ee7392419c58185504d1c872979778
hoan1324 <hoan3397@gmail.com>
2024-02-29 20:52:17 +0700

    no message

--- EndingUi/HappyEnding.java
+++ EndingUi/HappyEnding.java
@@ -1,112 +1,108 @@
-// package EndingUi;
+package EndingUi;
 
-// import java.net.URL;
+import java.net.URL;
 
-// import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;
 
-// import javax.swing.JLabel;
+import javax.swing.JLabel;
 
-// import javax.swing.JPanel;
-// import javax.swing.JTextArea;
-
-
-
-
-// import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 
 
 
-// public class BadEnding {
-//         public CardLayout cardLayout = new CardLayout();
+import java.awt.*;
+
+
+
+
+public class HappyEnding {
+        public CardLayout cardLayout = new CardLayout();
   
-//    public JPanel badEndingPanelSum;
+   public JPanel happyEndingPanelSum;
     
-//    public JPanel cardPanel;
-//    public ButtonEnding buttonEnding=new ButtonEnding();
-//     public JPanel badEndingPanel[]=new JPanel[4];
-//     String Ending[] = {
-//         "Bạn đã trượt đại học do vướng vào con đường cờ bạc.Bạn có muốn làm lại cuộc đời?",
-//         "Bạn đã trượt đại học do quá đắm chìm vào sự thú vị ,hay ho của game.Bạn có muốn làm lại cuộc đời?",
-//         "Bạn đã bị nghiện do vô tình dẵm vào kim tiêm của người nghiện.Bạn có muốn làm lại cuộc đời?",
-//         "Tôi không ngờ bạn lại ngủ quên trên chiến thắng lâu như vậy.Bạn đã không hoàn thành mục tiêu.Bạn có muốn làm lại cuộc đời?"
-// };
-//    JTextArea text[] =new JTextArea[4];
-//    JLabel img[]=new JLabel[4];
-//     String badURL[] = new String[30];
-//     public BadEnding(){
+   public JPanel cardPanel;
+   public ButtonEnding buttonEnding=new ButtonEnding();
+    public JPanel happyEndingPanel[]=new JPanel[4];
+    String Ending[] = {
+    "Chúc mừng bạn đã hoàn thành xuất sắc mục tiêu được đề ra! Thành thật mà nói, tôi khá bất ngờ vì không nghĩ bạn có thể đạt được kết quả cao đến vậy. Nhưng sự quyết tâm và kỷ luật của bạn trong suốt những năm tháng qua đã chứng minh điều ngược lại. Đó sẽ là chìa khóa giúp bạn tiếp bước trên con đường trưởng thành của mình.Nhiệm vụ của tôi với bạn đã hoàn thành. Chúc bạn sẽ trải qua khoảng thời gian đẹp đẽ thời sinh viên. Bây giờ, tôi sẽ tiếp tục hành trình của mình để giúp đỡ những người khác, đặc biệt là những học sinh có ước mơ ,có hoài bão như bạn. Bạn có muốn cùng tôi tham gia?",
+    "Wow, thật tuyệt vời! Tôi thực sự bất ngờ khi bạn hoàn thành mục tiêu xuất sắc  như vậy. Chắc chắn bạn sẽ trở thành một tinh anh của xã hội, góp phần xây dựng đất nước ngày càng phát triển.Nhiệm vụ của tôi với bạn đã hoàn thành. Bạn có muốn cùng tôi tiếp bước để giúp đỡ những người khác, đặc biệt là những học sinh có ước mơ ,có hoài bão như bạn? Tôi tin rằng với sự góp sức của bạn, mọi thử thách đều sẽ diễn ra suôn sẻ."
+};
+   JTextArea text[] =new JTextArea[2];
+   JLabel img[]=new JLabel[2];
+    String happyURL[] = new String[30];
+    public HappyEnding(){
       
-//         for(int i=0;i<4;i++){
-//         badEndingPanel[i] = new JPanel();
-//        badEndingPanel[i].setBounds(0,0,615,690);
-//        badEndingPanel[i].setOpaque(true);
-//        badEndingPanel[i].setVisible(true);
-//        badEndingPanel[i].setLayout(null);
-//        badEndingPanel[i].setBackground(new Color(145,147,148));
-//        }
+        for(int i=0;i<2;i++){
+    happyEndingPanel[i] = new JPanel();
+       happyEndingPanel[i].setBounds(0,0,615,690);
+       happyEndingPanel[i].setOpaque(true);
+       happyEndingPanel[i].setVisible(true);
+       happyEndingPanel[i].setLayout(null);
+       happyEndingPanel[i].setBackground(new Color(128 ,184, 233));
+       }
        
        
-//        badURL[0] ="./picture/cobacBadEnding.jpg";
-//        badURL[1] ="./picture/gameBadEnding.jpg";
-//        badURL[2] ="./picture/nghienBadEnding.jpg";
-//        badURL[3] ="./picture/fallScoreEnding.png";
+       happyURL[0] ="./picture/bgr.bng";
+
        
          
-//         setPanel();
-//         setUp();
-//     } 
-//     public void setPanel(){
-//         for(int i=0;i<4;i++){
-//             text[i]=new JTextArea(Ending[i]);
-//             text[i].setBounds(80,420,455,40);
-//             text[i].setEnabled(false);
-//             text[i].setBackground(new Color(145,147,148));
-//             text[i].setForeground(new Color(222,13,13));
-//             text[i].setAlignmentY(JTextArea.CENTER_ALIGNMENT);
-//             text[i].setLineWrap(true);
-//             text[i].setWrapStyleWord(true);
+        setPanel();
+        setUp();
+    } 
+    public void setPanel(){
+        for(int i=0;i<2;i++){
+            text[i]=new JTextArea(Ending[i]);
+            text[i].setBounds(80,420,455,40);
+            text[i].setEnabled(false);
+            text[i].setBackground(new Color(128 ,184, 233));
+            text[i].setForeground(new Color(222,13,13));
+            text[i].setAlignmentY(JTextArea.CENTER_ALIGNMENT);
+            text[i].setLineWrap(true);
+            text[i].setWrapStyleWord(true);
 
-//             ImageIcon newImage = new ImageIcon(new ImageIcon(badURL[i]).getImage().getScaledInstance(400,400, Image.SCALE_SMOOTH));
-//             img[i]=new JLabel();
-//             img[i].setIcon(newImage);
-//             img[i].setBounds(107,0,400,400);
-//             img[i].setVisible(true);
+            ImageIcon newImage = new ImageIcon(new ImageIcon(happyURL[0]).getImage().getScaledInstance(400,400, Image.SCALE_SMOOTH));
+            img[i]=new JLabel();
+            img[i].setIcon(newImage);
+            img[i].setBounds(107,0,400,400);
+            img[i].setVisible(true);
 
-//             badEndingPanel[i].add(text[i]);
-//             badEndingPanel[i].add(img[i]);
-//             badEndingPanel[i].setBackground(new Color(145,147,148));
-//         }
+            happyEndingPanel[i].add(text[i]);
+            happyEndingPanel[i].add(img[i]);
+            happyEndingPanel[i].setBackground(new Color(128 ,184, 233));
+        }
         
        
         
-//     }
-//     public void setUp(){
-//         cardPanel = new JPanel(); // Use JPanel instead of JLayeredPane
-//         cardPanel.setBounds(0, 0, 615, 690);
+    }
+    public void setUp(){
+        cardPanel = new JPanel(); // Use JPanel instead of JLayeredPane
+        cardPanel.setBounds(0, 0, 615, 690);
       
-//         // Sử dụng CardLayout cho cardPanel
-//         cardPanel.setLayout(cardLayout);
+        // Sử dụng CardLayout cho cardPanel
+        cardPanel.setLayout(cardLayout);
         
-//         for(int i=0;i<4;i++){
-//             cardPanel.add(badEndingPanel[i], "bad"+i);
+        for(int i=0;i<2;i++){
+            cardPanel.add(happyEndingPanel[i], "happy"+i);
             
             
-//         }
+        }
         
-//         // Thể hiện thẻ đầu tiên (ở đây là Intro)
+       
         
-//         cardLayout.show(cardPanel,"bad3" );
-//         badEndingPanelSum=new JPanel();
-//         badEndingPanelSum.setBounds(0,0,615,690);
-//         badEndingPanelSum.setOpaque(true);
-//         badEndingPanelSum.setVisible(true);
-//         badEndingPanelSum.setLayout(null);
-//         badEndingPanelSum.add(buttonEnding.YesButton);
-//         badEndingPanelSum.add(buttonEnding.NoButton);
-//         badEndingPanelSum.add(cardPanel);
+        cardLayout.show(cardPanel,"happy3" );
+        happyEndingPanelSum=new JPanel();
+        happyEndingPanelSum.setBounds(0,0,615,690);
+        happyEndingPanelSum.setOpaque(true);
+        happyEndingPanelSum.setVisible(true);
+        happyEndingPanelSum.setLayout(null);
+        happyEndingPanelSum.add(buttonEnding.YesButton);
+        happyEndingPanelSum.add(buttonEnding.NoButton);
+        happyEndingPanelSum.add(cardPanel);
         
 
-//     }
+    }
     
    
-// }
+}
