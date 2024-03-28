a8aff30d5c505e4a34b712390aa9ae14baa34080
hoan1324 <hoan3397@gmail.com>
2024-03-28 22:24:04 +0700

    upadaet mvc

---
+++ Controller/IntroController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import Model.IntroModel;
+import View.IntroView;
+import main.MyFrame;
+
+public class IntroController {
+    MyFrame Mf;
+    IntroModel introModel;
+    IntroView introView;
+    public IntroController(MyFrame Mf){
+        this.Mf=Mf;
+        introModel=new IntroModel(Mf);
+        introView=new IntroView(Mf, introModel);
+    }
+    public IntroModel getIntroModel() {
+        return introModel;
+    }
+    
+}
---
+++ Model/IntroModel.java
@@ -0,0 +1,36 @@
+package Model;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import main.MyFrame;
+
+public class IntroModel {
+    MyFrame Mf;
+    private JPanel introPanel;
+    private JButton Start;
+    private JButton Exit;
+    private JButton loginButton; 
+    public IntroModel(MyFrame Mf){
+        this.Mf=Mf;
+        introPanel=new JPanel();
+        Start=new JButton("Start");
+        Exit=new JButton("Exit");
+        loginButton=new JButton("Đăng nhập");
+    }
+    public JPanel getIntroPanel() {
+        return introPanel;
+    }
+
+    public JButton getStart() {
+        return Start;
+    }
+
+    public JButton getExit() {
+        return Exit;
+    }
+
+    public JButton getLoginButton() {
+        return loginButton;
+    }
+}
