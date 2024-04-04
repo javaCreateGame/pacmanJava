eb1e2bda094dc41b7ecb49c58c0452cf20abeaf5
hoan1324 <hoan3397@gmail.com>
2024-04-04 21:13:28 +0700

    update

--- main/MyFrame.java
+++ main/MyFrame.java
@@ -10,7 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import Controller.StateController.IntroController;
+
 import EndingUi.BadEnding;
 import EndingUi.HappyEnding;
 import Sound.SoundEffect;
